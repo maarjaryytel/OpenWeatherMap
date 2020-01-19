b127e68d9a3de5444c2fb6705c34f697ccb940f7
maarjaryytel <maarja@ryytel.eu>
2020-01-19 16:36:29 +0200

    OpenWeatherMap

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Net;
+
+namespace weather
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			string url = "http://api.openweathermap.org/data/2.5/weather?q=Tallinn&units=metric&appid=14b53ebd35439933fcee6c2d32830f60";
+
+			HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+			request.Method = "GET";
+
+			var webResponce = request.GetResponse();
+			var webStream = webResponce.GetResponseStream();
+
+			using (var responceReader = new StreamReader(webStream))
+			{
+				var responce = responceReader.ReadToEnd();
+				WeatherDetails.root weather = JsonConvert.DeserializeObject<WeatherDetails.root>(responce);
+				Console.WriteLine(weather.name);
+				Console.WriteLine(weather.sys.Country);
+				Console.WriteLine(weather.Weather[0].Main);
+				Console.WriteLine(weather.Weather[0].Description);
+				Console.WriteLine(weather.main.Temp);
+
+			}
+			
+		}
+	}
+}
---
+++ WeatherDetails.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace weather
+{
+	class WeatherDetails
+	{
+
+		public class Main
+		{
+			public double Temp { get; set; }			
+		}
+
+		public class Weather
+		{
+			public string Main { get; set; }
+			public string Description { get; set; }
+			
+		}
+
+		public class Sys
+		{
+			public string Country { get; set; }
+		}
+
+		public class root
+		{
+			public Sys sys { get; set; }
+			public List<Weather>Weather { get; set; }
+			public string name { get; set; }
+			public Main main { get; set; }
+		}
+
+	}
+}
